9203187bc247667078db691bb1774d1a1b0cf277
rafu01 <nrafsan13@gmail.com>
2023-04-09 15:11:38 +0600

    Add AOP annotation

--- src/main/java/com/dev/springaop/aop/Logger.java
+++ src/main/java/com/dev/springaop/aop/Logger.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 @Slf4j
@@ -13,6 +15,8 @@ public class Logger {
     private final String beforeLoggedIn;
     private final String afterLoggedIn;
 
+
+    @Before("execution(public boolean isEligibleForLogIn())")
     private void putLog(){
         log.info("Verification complete");
     }
--- src/main/java/com/dev/springaop/controller/DemoController.java
+++ src/main/java/com/dev/springaop/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.dev.springaop.controller;
 
 import com.dev.springaop.service.DemoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class DemoController {
 
     private final DemoService demoService;
 
     @GetMapping("/api/v1/login")
     public ResponseEntity<String> isEligible(@RequestBody String user){
+        log.info(user);
         demoService.isEligibleForLogIn(user);
-        return ResponseEntity.ok("Loaded");
+        return new ResponseEntity<>("Loaded", HttpStatus.OK);
     }
 }
